9cef38d494ff0fd2dba58de3b526bc7c8043de0b
Hirose <>
2018-12-17 18:40:39 +0900

    修正しました。

--- NewRobot/src/org/usfirst/frc/team6909/robot/Robot.java
+++ NewRobot/src/org/usfirst/frc/team6909/robot/Robot.java
@@ -15,16 +15,19 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
+
+
+
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends IterativeRobot {
 	private XboxController box_drive, box_lift;
-	private PWMTalonSRX rightarm, leftarm;
-	private Spark rightRear, rightFront,
-				   leftRear , leftFront;
-	private SpeedControllerGroup m_right,m_left;
+	private PWMTalonSRX arm_right, arm_left;
+	private Spark spark_rightRear, spark_rightFront,
+				   spark_leftRear , spark_leftFront;
+	private SpeedControllerGroup drive_right,drive_left;
 	private DifferentialDrive myDrive;
 	private Spark lift;
 
@@ -35,57 +38,83 @@ public class Robot extends IterativeRobot {
 
 	private double NoReact=0.1;
 
+	private double dataProcessing(double value) {
+
+		/*try {
+			Class<?> Class_box = XboxController.class;
+			Object Object_box = box;
+			Method Method_box = Class_box.getMethod(functionType, Hand.class);
+
+			value = (double) box.invoke(Method_box, hand);
+
+		}catch(ReflectiveOperationException e ) {
+
+		}*/
+
+
+		value *= Math.abs(value) > NoReact ? 1 : 0 ;
+
+		return value;
+	}; //不感帯未満なら0
+
 	@Override
 	public void robotInit() {
 		box_drive = new XboxController(0);
 		box_lift  = new XboxController(1);
 
-		rightarm = new PWMTalonSRX(5);
-		leftarm  = new PWMTalonSRX(6);
+		arm_right = new PWMTalonSRX(5);
+		arm_left  = new PWMTalonSRX(6);
 
-		rightRear  = new Spark(3);
-		rightFront = new Spark(2);
-		leftRear   = new Spark(1);
-		leftFront  = new Spark(0);
+		spark_rightRear  = new Spark(3);
+		spark_rightFront = new Spark(2);
+		spark_leftRear   = new Spark(1);
+		spark_leftFront  = new Spark(0);
 
-		m_right    = new SpeedControllerGroup(rightFront, rightRear);
-		m_left     = new SpeedControllerGroup(leftFront , leftRear);
+		drive_right = new SpeedControllerGroup(spark_rightFront, spark_rightRear);
+		drive_left  = new SpeedControllerGroup(spark_leftFront , spark_leftRear);
 
-		myDrive = new DifferentialDrive(m_right,m_left);
+		myDrive = new DifferentialDrive(drive_right, drive_left);
 
 		lift = new Spark(4);
 	}
 
 	@Override
 	public void teleopPeriodic() {
-	double armValue = 0;
 
-	XofDrive = box_drive.getX(Hand.kRight);
-	XofDrive *= Math.abs(XofDrive) > NoReact ? 1 : 0 ;
+	double arm_value = 0;
 
-	YofDrive = box_drive.getY(Hand.kLeft);
-	YofDrive *= Math.abs(YofDrive) > NoReact ? 1 : 0 ;
+	{
 
-	YofLift = box_lift.getY(Hand.kRight);
-	YofLift *= Math.abs(YofLift) > NoReact ? 1 : 0 ;
 
-	is_leftTriggerOn  = box_lift.getTriggerAxis(Hand.kLeft)  > NoReact;
-	is_rightTriggerOn = box_lift.getTriggerAxis(Hand.kRight) > NoReact;
+		XofDrive = dataProcessing(box_drive.getX(Hand.kRight));
 
-	if(is_leftTriggerOn && is_rightTriggerOn) {
-		armValue = 0;
-	}else {
-		armValue = is_leftTriggerOn ? -1.0 : armValue ;
-		armValue = is_rightTriggerOn ? 1.0 : armValue ;
+		YofDrive = dataProcessing(box_drive.getY(Hand.kLeft));
+
+		YofLift = dataProcessing(box_lift.getY(Hand.kRight));
+
+
+		is_leftTriggerOn  = box_lift.getTriggerAxis(Hand.kLeft)  > NoReact;
+		is_rightTriggerOn = box_lift.getTriggerAxis(Hand.kRight) > NoReact;
+
+		if(is_leftTriggerOn && is_rightTriggerOn) {
+			arm_value = 0;
+		}else {
+			arm_value = is_leftTriggerOn ? -1.0 : arm_value ;
+			arm_value = is_rightTriggerOn ? 1.0 : arm_value ;
 	}
 
+	}//データ処理
 
-	myDrive.arcadeDrive(YofDrive, -XofDrive);//直感と逆
+	{
+		myDrive.arcadeDrive(YofDrive, -XofDrive);//直感と逆
 
-	leftarm.set(armValue);
-	rightarm.set(-armValue);
+		arm_left.set(arm_value);
+		arm_right.set(-arm_value);
 
-	lift.set(-YofLift);//直感と逆
+		lift.set(-YofLift);//直感と逆
+
+	}//実行
 
 	}
+
 }
